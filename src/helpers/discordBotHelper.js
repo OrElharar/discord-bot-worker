2f0cc780ac75d4cb0c9c38a19898b00d1858302d
or <elhararor207@gmail.com>
2022-10-25 18:17:24 +0300

    1. Edited client.on("messageCreate"). moving a user to a voice channel is optional only if the user is in a voice channel (seems like discord limitations). 2. Added Todos.

---
+++ src/helpers/discordBotHelper.js
@@ -0,0 +1,75 @@
+const { Client, IntentsBitField, GatewayIntentBits} = require('discord.js');
+const  globals = require("../helpers/globalsHelper");
+
+const client = new Client({ intents: [IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+        GatewayIntentBits.GuildMembers] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}, Ready to go`);
+    console.log(client.users);
+});
+
+client.on("error", (err)=>{
+    console.log(`Discord Error: ${err.message}`);
+})
+
+client.on("messageCreate", async (msg)=>{
+    try {
+        console.log(msg.content) //For debugging
+        msg.content = isJsonValid(msg.content) ? JSON.parse(msg.content) : msg.content;
+        if (msg.content === "PING")
+            msg.channel.send("PONG")
+        // if(msg.author.id !== process.env.DISCORD_WEBHOOK_USER_ID) // Uncomment when debugging is done
+        //     return
+        if (msg.content.type === globals.CREATE_NEW_CHANNEL_MSG) {
+            const channel = await msg.guild.channels.create({
+                name: msg.content.data.channelName,
+                type: 2
+            });
+            console.log(`Created channel ${channel.name} successfully`)
+        }
+        if(msg.content.type === "$move"){
+            console.log("In $move");
+            //TODO - find a way fetch GuildMember object by userId
+            const discordUserId = msg.content.data.userId.slice(2, -1);
+            console.log({discordUserId})
+            // const test = msg.guild.members.cache.get(discordUserId); //failed
+            // console.log({test})
+            const member = client.members.cache.get(discordUserId);//failed
+            if(member == null) {
+                console.log({err: "Member was not mentioned"});
+                return;
+            }
+            const channelId = msg.guild.channels.cache.find(channel => channel.name === "The Voice Channel");
+            const res = await member.voice.setChannel(channelId);
+            console.log({channelId, res})
+        }
+
+    }catch(err){
+        console.log({errMessage: err.message});
+    }
+})
+
+
+
+const isJsonValid = (value)=>{
+    try{
+        JSON.parse(value);
+        return true;
+    }catch(err){
+        return false;
+    }
+}
+
+const login = async()=>{
+    client.login(process.env.DISCORD_BOT_TOKEN)
+        .then()
+        .catch((err)=> {
+            console.log("login to Discord bot Failed, trying again...", err)
+            login().then()
+        })
+}
+
+login().then(()=> console.log("Login successfully to Discord bot"))
--- src/routers/botRouter.js
+++ src/routers/botRouter.js
@@ -1,25 +1,26 @@
 const express = require("express");
-const discordHelper = require("../helpers/discordHelper");
+const discordApiHelper = require("../helpers/discordApiHelper");
+require("../helpers/discordBotHelper");
 // const auth = require("../middleware/auth");
 
 const router = new express.Router();
 
 router.post("/create-channel", async(req,res, next)=>{
-    const {err, response} = await discordHelper.sendCreateChannelMsg(req.body.channelName);
+    const {err, response} = await discordApiHelper.sendCreateChannelMsg(req.body.channelName);
     if(err != null)
         return next(err);
     return res.status(200).send({ data: response });
 });
 
 router.post("/move-member", async(req,res, next)=>{
-    const {err, response} = await discordHelper.sendCMoveMemberMsg(req.body.channelName, req.body.userId);
+    const {err, response} = await discordApiHelper.sendCMoveMemberMsg(req.body.channelName, req.body.userId);
     if(err != null)
         return next(err);
     return res.status(200).send({ data: response });
 });
 
 router.post("/add-message", async(req,res, next)=>{
-    const {err, response} = await discordHelper.sendMessage(req.body.message);
+    const {err, response} = await discordApiHelper.sendMessage(req.body.message);
     if(err != null)
         return next(err);
     return res.status(200).send({ data: response });
